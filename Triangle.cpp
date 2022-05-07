5eb61b3a3c33acef915a33fc0c2392f8cc2c230a
WDobies <68401136+WDobies@users.noreply.github.com>
2022-05-07 20:03:55 +0200

    Fix barycentric

--- Triangle.cpp
+++ Triangle.cpp
@@ -40,13 +40,17 @@ Vector3* Triangle::IntersectPoint(Ray& ray)
 {
 
 	Vector3 normal = n1;
+	Vector3 v1v2 = v2 - v1;
+	Vector3 v1v3 = v3 - v1;
+
 	if (&normal == NULL) {
-		Vector3 v1v2 = v2 - v1;
-		Vector3 v1v3 = v3 - v1;
-		
-		Vector3 normal = Vector3::Cross(v1v2, v1v3);
+		normal = Vector3::Cross(v1v2, v1v3);
 		normal = normal.Normalize();
 	}
+
+	Vector3 crossProd = Vector3::Cross(v1v2, v1v3);
+	float field = Vector3::Dot(crossProd, normal);
+
 	float dir = Vector3::Dot(normal, ray.direction);
 
 	if (dir > EPSILON) //backsided
@@ -61,46 +65,36 @@ Vector3* Triangle::IntersectPoint(Ray& ray)
 	if (t < 0)
 		return nullptr; // ray behind the triangle
 
-
 	Vector3 point = ray.RayStep(t);
 
 	//v1
 	Vector3 e1 = v2 - v1;
 	Vector3 vp1 = point - v1;
 	Vector3 c1 = Vector3::Cross(e1, vp1);
+	float fieldv1 = Vector3::Dot(c1, normal);
 
-	if (Vector3::Dot(normal, c1) < EPSILON)
+	float alpha = fieldv1 / field;
+	if (alpha >= 1 || alpha <= 0)
 		return nullptr;
 
 	Vector3 e2 = v3 - v2;
 	Vector3 vp2 = point - v2;
 	Vector3 c2 = Vector3::Cross(e2, vp2);
+	float fieldv2 = Vector3::Dot(c2, normal);
 
-	if (Vector3::Dot(normal, c2) < EPSILON)
+	float beta = fieldv2 / field;
+	if (beta >= 1 || beta <= 0)
 		return nullptr;
 
 	//v3
 	Vector3 e3 = v1 - v3;
 	Vector3 vp3 = point - v3;
 	Vector3 c3 = Vector3::Cross(e3, vp3);
-
-	if (Vector3::Dot(normal, c3) < EPSILON)
-		return nullptr;
-
-	Vector3 ev0 = v1 - v3;
-	Vector3 ev1 = v1 - v2;
-
-	Vector3 temp = Vector3::Cross(ev1, ev0);
-	float field = Vector3::Dot(temp, normal);
-
-	float fieldv1 = Vector3::Dot(c1, normal);
-	float fieldv2 = Vector3::Dot(c2, normal);
 	float fieldv3 = Vector3::Dot(c3, normal);
 
-	float alpha = fieldv1 / field;
-	float beta = fieldv2 / field;
 	float theta = fieldv3 / field;
-	//std::cout << theta << "\n";
+	if (theta >= 1 || theta <= 0)
+		return nullptr;
 
 	return new Vector3(ray.RayStep(t));
 }
